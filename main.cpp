fb828cc70d84145f417d31b512476c872f983bad
Vishal2186 <vishal.609.vishal.609@gmail.com>
2025-01-12 15:42:01 +0530

    add

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int cen__code(int cen)
+{
+    if(((cen+100)/100)%4 == 2){return 4;}
+   else if((cen/100)%4==0){return 6;}
+   else if((cen/100)%2==0){return 2;}
+   else{return 0; }
+    
+}
+int week_day(int d,string m,int y,int cen)
+{
+    string w_days[7] = {"sunday","Monday","Tuesday","wednesday","Thursday","Friday","saturday"};
+    string arr1[12] = {"jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"};
+    int arr2[12] = {0,3,3,6,1,4,6,2,5,0,3,5};
+    int month_code;
+    
+    for(int i = 0; i<12;i++)
+    {
+        if(arr1[i] == m){month_code = i;break;}
+    }
+     int odd_days = ( d + arr2[month_code] + cen__code(cen) + y + (y/4) ) % 7;
+     if((m == "jan" || m == "feb") && y == 4)
+     {
+        cout<<w_days[odd_days-1]<<endl;
+     }
+        else{cout<<w_days[odd_days]<<endl;}
+    cout<<endl;cout<<endl;cout<<endl;
+     return 0;
+}
+
+int main(){
+    int date,year,cen;
+    string month;
+    cout<<endl;
+    cout<<"\t\t***---W E E K  D A Y  C A L C U L A T O R---***"<<endl;
+    cout<<endl;
+    cout<<"Enter date = ";cin>>date;
+    cout<<endl;
+    cout<<"(like this 'jan-dec')"<<endl;
+    cout<<"Enter month = ";cin>>month;
+    cout<<endl;
+    cout<<"(like this '1-99' )"<<endl;
+    cout<<"Enter year = ";cin>>year;
+    cout<<endl;
+    cout<<"(1100-2100 0r any other)"<<endl;
+    cout<<"Enter century = ";cin>>cen;
+    cout<<endl;cout<<endl;
+    cout<<"On "<<date<<" "<<month<<", "<<cen+year<<" the day was: "<<week_day(date,month,year,cen);
+    cout<<"***************---------------E N D---------------***************";
+    return 0;
+}
